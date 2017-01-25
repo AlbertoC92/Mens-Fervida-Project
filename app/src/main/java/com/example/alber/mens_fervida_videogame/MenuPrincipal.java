1963a18adb78d482f6f9bd249079fbf696d6fa2c
AlbertoC92 <alberto.g.castro92@gmail.com>
2017-01-25 09:43:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alber/mens_fervida_videogame/MenuPrincipal.java
@@ -0,0 +1,13 @@
+package com.example.alber.mens_fervida_videogame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
