e758f24ab6f0a74aac642e0fe2a6c44fbba98baa
pedromi92 <pedromi92@gmail.com>
2017-01-26 21:41:29 +0100

    Modificado Icono del launcher

--- app/src/main/java/com/example/alber/mens_fervida_videogame/AvataresAdapter.java
+++ app/src/main/java/com/example/alber/mens_fervida_videogame/AvataresAdapter.java
@@ -17,7 +17,7 @@ public class AvataresAdapter extends BaseAdapter {
     private Integer[] caracteres = {
             R.drawable.avatar1,R.drawable.avatar2, R.drawable.avatar3,
             R.drawable.avatar4,R.drawable.avatar5, R.drawable.avatar6,
-            R.drawable.avatar7,R.drawable.avatar8,
+            R.drawable.avatar7,R.drawable.avatar8
     };
     public AvataresAdapter(Context c) {
         mContext = c;
