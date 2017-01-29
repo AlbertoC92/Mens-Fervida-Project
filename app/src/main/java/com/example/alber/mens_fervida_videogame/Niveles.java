b8e294197eb3d6b0074ff909c1432247d0271cdd
Jesús Pedro Sánchez <jesus_pedro88@hotmail.com>
2017-01-29 17:48:06 +0100

    Layout del número de niveles del juego

---
+++ app/src/main/java/com/example/alber/mens_fervida_videogame/Niveles.java
@@ -0,0 +1,45 @@
+package com.example.alber.mens_fervida_videogame;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Niveles extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//Línea para ocultar el titulo
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);//Línea para ocultar la barra de información de la batería,etc...
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveles);
+        //comentario
+    }
+
+
+    public void nivel1(View view){
+
+    }
+
+    public void nivel2(View view){
+
+    }
+
+    public void nivel3(View view){
+
+    }
+
+    public void nivel4(View view){
+
+    }
+
+    public void nivel5(View view){
+
+    }
+
+    public void volver(View view){
+        finish();
+    }
+}
